d877bf10bdc4927c0fa3c10dfd95bc075d074614
EvgeniyTLT <Toy444@ya.ru>
2022-08-04 00:14:36 +0300

    add

--- app/src/main/java/com/example/plantshandbook/PlantAdapter.kt
+++ app/src/main/java/com/example/plantshandbook/PlantAdapter.kt
@@ -3,8 +3,10 @@ package com.example.plantshandbook
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 
-class PlantAdapter: RecyclerView.Adapter<PlantAdapter.PlantHolder> {
-    class PlantHolder(item: View): RecyclerView.ViewHolder(item) {
+class PlantAdapter : RecyclerView.Adapter<PlantAdapter.PlantHolder> {
+    class PlantHolder(item: View) : RecyclerView.ViewHolder(item) {
+        fun bind(plant: Plant) {
 
+        }
     }
 }
