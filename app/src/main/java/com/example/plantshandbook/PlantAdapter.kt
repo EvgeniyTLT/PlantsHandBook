a237c4b70decf610283740d0a147ab529bf3f578
EvgeniyTLT <Toy444@ya.ru>
2022-08-04 00:09:55 +0300

    Initial commit

---
+++ app/src/main/java/com/example/plantshandbook/PlantAdapter.kt
@@ -0,0 +1,10 @@
+package com.example.plantshandbook
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class PlantAdapter: RecyclerView.Adapter<PlantAdapter.PlantHolder> {
+    class PlantHolder(item: View): RecyclerView.ViewHolder(item) {
+
+    }
+}
