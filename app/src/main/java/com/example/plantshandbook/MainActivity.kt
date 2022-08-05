8abd6b2f9318a7f8ae4adfe1cf50077fc96bb1eb
EvgeniyTLT <Toy444@ya.ru>
2022-08-05 17:07:23 +0300

    add

--- app/src/main/java/com/example/plantshandbook/MainActivity.kt
+++ app/src/main/java/com/example/plantshandbook/MainActivity.kt
@@ -10,6 +10,14 @@ import com.example.plantshandbook.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
     private val adapter = PlantAdapter()
+    private val imageIdList = listOf(
+        R.drawable.plant1,
+        R.drawable.plant2,
+        R.drawable.plant3,
+        R.drawable.plant4,
+        R.drawable.plant5,
+    )
+    private var index = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,6 +30,12 @@ class MainActivity : AppCompatActivity() {
         binding.apply {
             rcView.layoutManager = GridLayoutManager(this@MainActivity, 3)
             rcView.adapter = adapter
+            buttonAdd.setOnClickListener {
+                if (index > 4) index = 0
+                val plant = Plant(imageIdList[index], "Plant $index")
+                adapter.addPlant(plant)
+                index++
+            }
         }
     }
 }
